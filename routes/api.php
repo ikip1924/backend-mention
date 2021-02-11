8e4cce3746a28d26f3653c2a9b4227ce4718db7d
Rifky <rifkyrahma0420@gmail.com>
2021-02-11 14:13:58 +0700

    backend autentikasi user dengan sanctum

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Auth\{
+    RegisterController,
+    LoginController,
+    MeController,
+    LogoutController
+};
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', MeController::class);
+    Route::post('logout', LogoutController::class);
+});
